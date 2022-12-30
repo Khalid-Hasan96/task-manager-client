2772d34ade7825fb07382339d756a66f36104187
Khalid-Hasan96 <khbappy66@gmail.com>
2022-12-30 19:41:44 +0600

    auth added

--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,51 +1,67 @@
-import React from 'react';
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Input,
-    Checkbox,
-    Button,
-} from "@material-tailwind/react";
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import google from '../../assets/google.png';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Signup = () => {
+    const { createUser } = useContext(AuthContext);
+
+    const handleSignup = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(res => {
+                const user = res.user;
+                console.log(user)
+            })
+            .catch(err => console.error(err))
+    }
+
     return (
-        <Card className='bg w-full lg:max-w-screen-2xl mx-auto my-5'>
-            <CardHeader
-                variant="gradient"
-                color="blue"
-                className="my-4 grid h-20 place-items-center"
-            >
-                <Typography variant="h3" color="white">
-                    Sign up
-                </Typography>
-            </CardHeader>
-            <CardBody className="flex flex-col gap-4">
-                <Input label="Name" size="lg" />
-                <Input label="Email" size="lg" />
-                <Input label="Password" size="lg" />
-            </CardBody>
-            <CardFooter className="pt-0">
-                <Button variant="gradient" fullWidth>
-                    Sign up
-                </Button>
-                <Typography variant="small" className="mt-6 flex justify-center">
-                    Already have an account?
-                    <Typography
-                        as="a"
-                        href="#signup"
-                        variant="small"
-                        color="blue"
-                        className="ml-1 font-bold"
-                    >
-                        <Link to='/login'>Login</Link>
-                    </Typography>
-                </Typography>
-            </CardFooter>
-        </Card>
+        <section className='bg w-full lg:max-w-screen-2xl mx-auto my-5 p-10'>
+            <h2 className='text-center w-1/5 mx-auto mb-5 font-bold text-3xl border border-gray-500 rounded-lg py-4'>SIGNUP</h2>
+            <form onSubmit={handleSignup}>
+                <label className="block">
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                        Name
+                    </span>
+                    <input type="text" name="name" className="mt-1 px-3 py-2 bg-white border shadow-xl border-black placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="John Doe" />
+                </label>
+                <label className="block my-6">
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                        Email
+                    </span>
+                    <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-xl border-black placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
+                </label>
+                <label className="block mb-6">
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                        Password
+                    </span>
+                    <input type="password" name="password" className="mt-1 px-3 py-2 bg-white border shadow-xl border-black placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="password123456" />
+                </label>
+                <label className="block">
+                    <input type="submit" className="px-3 py-2 shadow-xl block w-2/5 bg-gray-900 text-white font-semibold text-xl hover:bg-gray-500 hover:text-black ease-in duration-300 border border-slate-500 mx-auto rounded-md" value="Signup" />
+                </label>
+            </form>
+            <div className="relative flex py-5 items-center">
+                <div className="flex-grow border-t border-gray-400"></div>
+                <span className="flex-shrink mx-4 text-gray-400">OR</span>
+                <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+            <div>
+                <button className='bg-white border border-gray-600 hover:bg-green-600 ease-in duration-300 px-4 py-2 rounded-xl font-semibold flex items-center mx-auto'>
+                    <img src={google} alt="" />
+                    <span className='ml-2'>Google</span>
+                </button>
+            </div>
+            <div className='text-center mt-5'>
+                <p>Already have an account? <Link to='/login' className='font-semibold underline underline-offset-4 text-blue-700'>Login</Link></p>
+            </div>
+        </section>
     );
 };
 
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,13 +1,45 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const authInfo = {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        createUser,
+        login,
+        logOut,
+        loading
     }
 
     return (
